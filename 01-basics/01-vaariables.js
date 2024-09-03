adc0e2734dc756861ef237698838e9f24187b05b
nouraameto <nouraameto@gmail.com>
2024-09-03 19:23:19 +0300

    new file

---
+++ 01-basics/01-vaariables.js
@@ -0,0 +1,10 @@
+const accountId = 144553;
+let accountEmail = "nourahindi@game.com";
+var accountPassword = "12345" ;
+// accountId = 2
+/*
+perfer not to use var 
+because of issue in block scope and funtional scope
+*/
+console.table([accountId, accountEmail, accountPassword]);
+
