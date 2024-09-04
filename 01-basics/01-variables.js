20ec445fe3c27ac45c27c97e4e74143bffcf51b4
nouraameto <nouraameto@gmail.com>
2024-09-04 18:24:36 +0300

    data type

--- 01-basics/01-variables.js
+++ 01-basics/01-variables.js
@@ -3,4 +3,5 @@ const age = 29;
 let email = 'noura29@gmail.com';
 let city = 'makkah';
 let compny = 'youtube';
-console.table([name, age, email, city, compny]);
+let stuta;
+console.table([name, age, email, city, compny, stuta]);
---
+++ 01-basics/01_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3); // we are using nodejs, not browser 
+
+console.log(3
+    + 
+3); // Code readability should be high
+
+console.log('nora');
+
+let name = 'nora'; // string data type 
+const age = 29; // number data type 
+let siLoggedIn = false; // boolean data type 
+let state = null; // null => is standalone value 
+
+// object 
+console.log(typeof null);
+console.log(typeof name);
