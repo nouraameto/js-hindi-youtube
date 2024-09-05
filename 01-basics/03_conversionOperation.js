84a9f1b0b9dacb3186a6b52ad31407416221d293
nouraameto <nouraameto@gmail.com>
2024-09-05 17:15:11 +0300

    JavaScript Operation

---
+++ 01-basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = 33; 
+console.log(typeof(score));
+
+let isLoggedIn = "";
+let booleanIsLoddedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoddedIn);
+
+
+// ************* Operations *********************
+let value = 3 
+let negValue = -value;
+console.log(negValue);
+
+console.log(1 + 3 + "3");
+console.log("3" + 3 + 1);
+console.log((3 + 4) * 3 + 2);
+
+
+
